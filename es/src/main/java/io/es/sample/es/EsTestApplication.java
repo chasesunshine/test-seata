7f0fe2bfd14ce2a4523516b2f91e8ece18a7c724
chasesunshine <49563152+chasesunshine@users.noreply.github.com>
2021-08-16 10:55:26 +0800

    Update EsTestApplication.java

--- es/src/main/java/io/es/sample/es/EsTestApplication.java
+++ es/src/main/java/io/es/sample/es/EsTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EsTestApplication {
-    //测试123
+    //测试123456
     public static void main(String[] args) {
         SpringApplication.run(EsTestApplication.class, args);
     }
